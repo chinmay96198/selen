540ed795441be8743d97d947996fa519a2525fd0
chinmay96198 <chinmaysawant1999@gmail.com>
2025-02-05 16:18:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const card = document.getElementById("card");
+  
+    card.addEventListener("click", function () {
+      card.classList.toggle("flipped");
+    });
+  });
+  
